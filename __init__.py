b1efec9e0091aa29b268b422391f2841deb713c2
DuckMasterAl <43557963+DuckMasterAl@users.noreply.github.com>
2020-09-04 15:12:47 -0400

    Updated Function for Simplicity Purposeses

--- __init__.py
+++ __init__.py
@@ -4,21 +4,16 @@ class TopXyz:
     def __init__(self, token):
         self.token = token
 
-    async def post_stats(self, bot_id: str, guild_count:int, shard_count:int=None):
+    async def post_stats(self, bot_id, guild_count:int, shard_count:int=None):
         if shard_count is None:
-            async with aiohttp.ClientSession() as session:
-                async with session.post('https://topcord.xyz/api/bot/stats/' + bot_id,
-                headers={'Authorization': str(self.token)},
-                data=json.dumps({"guilds": guild_count, "shards": shard_count})) as r:
-                    return await r.json()
-        else:
-            async with aiohttp.ClientSession() as session:
-                async with session.post('https://topcord.xyz/api/bot/stats/' + bot_id,
-                headers={'Authorization': str(self.token)},
-                data=json.dumps({"guilds": guild_count, "shards": 0})) as r:
-                    return await r.json()
-
-    async def get_stats(self, bot_id: str):
+            shard_count = 0
         async with aiohttp.ClientSession() as session:
-            async with session.get('https://topcord.xyz/api/bot/stats/' + bot_id) as r:
+            async with session.post('https://topcord.xyz/api/bot/stats/' + str(bot_id),
+            headers={'Authorization': str(self.token)},
+            data=json.dumps({"guilds": guild_count, "shards": shard_count})) as r:
+                return await r.json()
+
+    async def get_stats(self, bot_id):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://topcord.xyz/api/bot/' + str(bot_id)) as r:
                 return await r.json()
